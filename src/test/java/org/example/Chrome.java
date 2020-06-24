f75c5a327755cb9693dce718ccf73bbd7ec467c5
1hareshsoftware <1hareshsoftware@gmail.com>
2020-06-24 22:16:07 +0530

    Automation Maven

---
+++ src/test/java/org/example/Chrome.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import com.sun.xml.internal.ws.resources.UtilMessages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Chrome {
+
+    static WebDriver driver;
+
+    public static void main(String[] args)
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\Soft\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://demo.nopcommerce.com/");
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.close();
+
+
+    }
+
+}
